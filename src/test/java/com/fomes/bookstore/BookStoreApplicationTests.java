9d0b61ead04ba78dabb31a3b7ddb12034573a3d4
fomes <fgomesdeluna@gmail.com>
2023-07-27 22:49:10 -0300

    Spring init

---
+++ src/test/java/com/fomes/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fomes.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
